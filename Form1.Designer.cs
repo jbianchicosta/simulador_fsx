b12e34a2f090c9e3bbd44a7ab0a5c4ab9a68ec3e
jbianchicosta <jbianchicosta@gmail.com>
2016-07-06 00:48:59 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Conectar FSX";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(119, 25);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Movimento Esquerdo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Movimento Direito";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(4, 155);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(109, 96);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Movimento Acima Profundor";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(119, 155);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(108, 96);
+            this.richTextBox3.TabIndex = 6;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(233, 155);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(108, 96);
+            this.richTextBox4.TabIndex = 7;
+            this.richTextBox4.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(104, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Movimento Abaixo Profundor";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(119, 282);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(108, 96);
+            this.richTextBox5.TabIndex = 9;
+            this.richTextBox5.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(126, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Movimento Centro";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Location = new System.Drawing.Point(382, 25);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(136, 96);
+            this.richTextBox6.TabIndex = 11;
+            this.richTextBox6.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(430, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Altidude";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(398, 360);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Desconectar FSX";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(346, 331);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Buscar  Dados";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.Location = new System.Drawing.Point(382, 155);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.Size = new System.Drawing.Size(136, 96);
+            this.richTextBox7.TabIndex = 15;
+            this.richTextBox7.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(415, 139);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Velocidade";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 395);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Simulador FSX";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+// Add these two statements to all SimConnect clients
+using Microsoft.FlightSimulator.SimConnect;
+using System.Runtime.InteropServices;
+using System.Collections;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        // User-defined win32 event
+        const int WM_USER_SIMCONNECT = 0x0402;
+
+        // SimConnect object
+        SimConnect simconnect = null;
+
+        enum DEFINITIONS
+        {
+            Struct1,
+        }
+
+        enum DATA_REQUESTS
+        {
+            REQUEST_1,
+        };
+
+        // this is how you declare a data structure so that
+        // simconnect knows how to fill it/read it.
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+        struct Struct1
+        {
+            // this is how you declare a fixed size string
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public String title;
+            public double latitude;
+            public double longitude;
+            public double altitude;
+            public int y;
+            public int x;
+            public int z;
+            public double velocidade;
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Simconnect client will send a win32 message when there is 
+        // a packet to process. ReceiveMessage must be called to
+        // trigger the events. This model keeps simconnect processing on the main thread.
+        // Usado pela API para criar conexao no FSX
+        protected override void DefWndProc(ref Message m)
+        {
+            if (m.Msg == WM_USER_SIMCONNECT)
+            {
+                if (simconnect != null)
+                {
+                    simconnect.ReceiveMessage();
+                }
+            }
+            else
+            {
+                base.DefWndProc(ref m);
+            }
+        }
+
+        private void closeConnection()
+        {
+            if (simconnect != null)
+            {
+                // Dispose serves the same purpose as SimConnect_Close()
+                simconnect.Dispose();
+                simconnect = null;
+                displayText("Conexao fechada");
+            }
+        }
+
+        // Set up all the SimConnect related data definitions and event handlers
+        // Recebe todoas as variaveis da aeronave e guarda em suas variaveis
+        private void initDataRequest()
+        {
+            try
+            {
+                // listen to connect and quit msgs
+                simconnect.OnRecvOpen += new SimConnect.RecvOpenEventHandler(simconnect_OnRecvOpen);
+                simconnect.OnRecvQuit += new SimConnect.RecvQuitEventHandler(simconnect_OnRecvQuit);
+
+                // listen to exceptions
+                simconnect.OnRecvException += new SimConnect.RecvExceptionEventHandler(simconnect_OnRecvException);
+
+                // define a data structure
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "Title", null, SIMCONNECT_DATATYPE.STRING256, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "Plane Latitude", "degrees", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "Plane Longitude", "degrees", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "Plane Altitude", "feet", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "YOKE Y POSITION", null, SIMCONNECT_DATATYPE.FLOAT64, 2.1f, SimConnect.SIMCONNECT_UNUSED);
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "YOKE X POSITION", null, SIMCONNECT_DATATYPE.FLOAT64, 2.1f, SimConnect.SIMCONNECT_UNUSED);
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "YOKE Z POSITION", null, SIMCONNECT_DATATYPE.FLOAT64, 2.1f, SimConnect.SIMCONNECT_UNUSED);
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "TOTAL VELOCITY", "feet", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+
+
+
+                // IMPORTANT: register it with the simconnect managed wrapper marshaller
+                // if you skip this step, you will only receive a uint in the .dwData field.
+                simconnect.RegisterDataDefineStruct<Struct1>(DEFINITIONS.Struct1);
+
+                // catch a simobject data request
+                simconnect.OnRecvSimobjectDataBytype += new SimConnect.RecvSimobjectDataBytypeEventHandler(simconnect_OnRecvSimobjectDataBytype);
+            }
+            catch (COMException ex)
+            {
+                displayText(ex.Message);
+            }
+        }
+
+        void simconnect_OnRecvOpen(SimConnect sender, SIMCONNECT_RECV_OPEN data)
+        {
+            displayText("Conectedo ao FSX");
+        }
+
+        // caso o usuario fechar o FSX
+        void simconnect_OnRecvQuit(SimConnect sender, SIMCONNECT_RECV data)
+        {
+            displayText("FSX foi fechado");
+            closeConnection();
+        }
+
+        void simconnect_OnRecvException(SimConnect sender, SIMCONNECT_RECV_EXCEPTION data)
+        {
+            displayText("Exception received: " + data.dwException);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (simconnect == null)
+            {
+                try
+                {
+                    // the constructor is similar to SimConnect_Open in the native API
+                    simconnect = new SimConnect("Managed Data Request", this.Handle, WM_USER_SIMCONNECT, null, 0);
+
+                    initDataRequest();
+                    timer1.Start();
+
+
+                }
+                catch (COMException ex)
+                {
+                    displayText("Nao foi possivel conectar ao FSX");
+                }
+            }
+            else
+            {
+                displayText("Error - try again");
+                closeConnection();
+
+             //   setButtons(true, false, false);
+            }
+        }
+               
+
+        void simconnect_OnRecvSimobjectDataBytype(SimConnect sender, SIMCONNECT_RECV_SIMOBJECT_DATA_BYTYPE data)
+        {
+            switch ((DATA_REQUESTS)data.dwRequestID)
+            {
+                case DATA_REQUESTS.REQUEST_1:
+                    Struct1 s1 = (Struct1)data.dwData[0];
+
+                    // displayText("Title: " + s1.title);
+                    //displayText("Lat:   " + s1.latitude);
+                    //displayText("Lon:   " + s1.longitude);
+                    //displayText("Alt:   " + s1.altitude);
+                    TextBox1("" + s1.y);
+                    TextBox2("" + s1.x);
+                    //TextBox3("" + s1.z);
+                    
+                    if (s1.y <= 0) {
+                        TextBox4("" + s1.x);
+                    }
+
+                    if (s1.x <= 0)
+                    {
+                        TextBox5("" + s1.x);
+                    }
+                    TextBox6("" + s1.altitude);
+                    TextBox7("" + s1.velocidade);
+
+                    break;
+
+                default:
+                    displayText("Unknown request ID: " + data.dwRequestID);
+                    break;
+            }
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (simconnect != null)
+            {
+                // Dispose serves the same purpose as SimConnect_Close()
+                simconnect.Dispose();
+                simconnect = null;
+                //displayText("Connection closed");
+                timer1.Stop();
+                closeConnection();
+                MessageBox.Show("Conexao fechada");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // The following call returns identical information to:
+             //simconnect.RequestDataOnSimObject(DATA_REQUESTS.REQUEST_1, DEFINITIONS.Struct1, SimConnect.SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD.ONCE);
+
+            simconnect.RequestDataOnSimObjectType(DATA_REQUESTS.REQUEST_1, DEFINITIONS.Struct1, 1, SIMCONNECT_SIMOBJECT_TYPE.USER);
+
+            //displayText("Request sent...");
+
+        }
+
+        // Response number
+        int response = 1;
+
+        // Output text - display a maximum of 10 lines
+        string output = "\n\n\n\n\n\n\n\n\n\n";
+
+        void displayText(string s)
+        {
+            // remove first string from output
+            output = output.Substring(output.IndexOf("\n") + 1);
+
+            // add the new string
+            output += "\n" + response++ + ": " + s;
+
+            // display it
+            richTextBox6.Text = output;
+        }
+
+        void TextBox1(string s)
+        {
+            richTextBox1.Text = s;
+        }
+
+        void TextBox2(string s)
+        {
+            richTextBox2.Text = s;
+        }
+
+        void TextBox3(string s)
+        {
+            richTextBox3.Text = s;
+        }
+
+        void TextBox4(string s)
+        {
+            richTextBox4.Text = s;
+        }
+
+        void TextBox5(string s)
+        {
+            richTextBox5.Text = s;
+        }
+
+        void TextBox6(string s)
+        {
+            // remove first string from output
+            // output = output.Substring(output.IndexOf("\n"));
+
+            // add the new string
+            //output += "\n" + response++ + ": " + s;
+            //output = s;
+
+            // mostra os dados
+            richTextBox6.Text = s;
+
+            /*ArrayList linhas = new ArrayList(richTextBox6.Lines);
+            // ordena as linhas
+            linhas.Sort();
+            //richTextBox6.Clear();
+            richTextBox6.Lines = (String[])linhas.ToArray(typeof(string));*/
+        }
+        void TextBox7(string s)
+        {
+            richTextBox7.Text = s;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            simconnect.RequestDataOnSimObjectType(DATA_REQUESTS.REQUEST_1, DEFINITIONS.Struct1, 0, SIMCONNECT_SIMOBJECT_TYPE.USER);
+            displayText("Request sent...");
+        }
+
+               private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            closeConnection();
+        }
+    }
+}
