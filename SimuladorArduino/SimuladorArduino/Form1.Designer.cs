4a050b42c29387387f07235b88b4f5ade0595baa
jbianchicosta <jbianchicosta@gmail.com>
2016-07-25 02:44:25 -0300

    Fsx Porta Serial
    Nesse projeto é enviado a porta serial do Arduíno os ângulos da aeronave.

---
+++ SimuladorArduino/SimuladorArduino/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace SimuladorArduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Envia Dados";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(324, 234);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Parar Envio";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(314, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(88, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(202, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Seleciona Porta Com";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 77);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(134, 77);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Profundor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(166, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Aileron";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(265, 77);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox3.TabIndex = 8;
+            this.richTextBox3.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(299, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Logs";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 264);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Simulador Serial";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SimuladorArduino/SimuladorArduino/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Text;
+using System.Windows.Forms;
+// Add these two statements to all SimConnect clients
+using Microsoft.FlightSimulator.SimConnect;
+using System.Runtime.InteropServices;
+
+namespace SimuladorArduino
+{
+    public partial class Form1 : Form
+    {
+        // User-defined win32 event
+        const int WM_USER_SIMCONNECT = 0x0402;
+
+        // SimConnect object
+        SimConnect simconnect = null;
+
+        enum DEFINITIONS
+        {
+            Struct1,
+        }
+
+        enum DATA_REQUESTS
+        {
+            REQUEST_1,
+        };
+
+        // this is how you declare a data structure so that
+        // simconnect knows how to fill it/read it.
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+        struct Struct1
+        {
+            // this is how you declare a fixed size string
+            //[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            //public String title;
+            //public double latitude;
+            //public double longitude;
+            //public double altitude;
+            //public int y;
+            public int a;
+            public int b;
+            //public int abc;
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            buscarPortaSerial();
+        }
+
+        // Simconnect client will send a win32 message when there is 
+        // a packet to process. ReceiveMessage must be called to
+        // trigger the events. This model keeps simconnect processing on the main thread.
+        // Usado pela API para criar conexao no FSX
+        protected override void DefWndProc(ref Message m)
+        {
+            if (m.Msg == WM_USER_SIMCONNECT)
+            {
+                if (simconnect != null)
+                {
+                    simconnect.ReceiveMessage();
+                }
+            }
+            else
+            {
+                base.DefWndProc(ref m);
+            }
+        }
+
+        private void closeConnection()
+        {
+            if (simconnect != null)
+            {
+                // Dispose serves the same purpose as SimConnect_Close()
+                simconnect.Dispose();
+                simconnect = null;
+                displayText("Conexao fechada");
+            }
+        }
+
+        // Set up all the SimConnect related data definitions and event handlers
+        // Recebe todoas as variaveis da aeronave e guarda em suas variaveis
+        private void initDataRequest()
+        {
+            try
+            {
+                // listen to connect and quit msgs
+                simconnect.OnRecvOpen += new SimConnect.RecvOpenEventHandler(simconnect_OnRecvOpen);
+                simconnect.OnRecvQuit += new SimConnect.RecvQuitEventHandler(simconnect_OnRecvQuit);
+
+                // listen to exceptions
+                simconnect.OnRecvException += new SimConnect.RecvExceptionEventHandler(simconnect_OnRecvException);
+
+                // define a data structure
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "PLANE PITCH DEGREES", "degrees", SIMCONNECT_DATATYPE.INT32, 0, SimConnect.SIMCONNECT_UNUSED);
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "PLANE BANK DEGREES", "degrees", SIMCONNECT_DATATYPE.INT32, 0, SimConnect.SIMCONNECT_UNUSED);
+
+                // IMPORTANT: register it with the simconnect managed wrapper marshaller
+                // if you skip this step, you will only receive a uint in the .dwData field.
+                simconnect.RegisterDataDefineStruct<Struct1>(DEFINITIONS.Struct1);
+
+                // catch a simobject data request
+                simconnect.OnRecvSimobjectDataBytype += new SimConnect.RecvSimobjectDataBytypeEventHandler(simconnect_OnRecvSimobjectDataBytype);
+            }
+            catch (COMException ex)
+            {
+                displayText(ex.Message);
+            }
+        }
+
+        void simconnect_OnRecvOpen(SimConnect sender, SIMCONNECT_RECV_OPEN data)
+        {
+            displayText("Conectedo ao FSX");
+        }
+
+        // caso o usuario fechar o FSX
+        void simconnect_OnRecvQuit(SimConnect sender, SIMCONNECT_RECV data)
+        {
+            displayText("FSX foi fechado");
+            closeConnection();
+        }
+        // tratar erros
+        void simconnect_OnRecvException(SimConnect sender, SIMCONNECT_RECV_EXCEPTION data)
+        {
+            displayText("Exception received: " + data.dwException);
+        }
+
+        void buscarPortaSerial()
+        {
+            String[] ports = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+                {
+                //serialPort1.PortName = "COM4";
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.Open();
+                serialPort1.BaudRate = 9600;
+                button1.Enabled = false;
+                button2.Enabled = true;
+                richTextBox1.ReadOnly = true;
+                richTextBox2.ReadOnly = true;
+                //timer1.Start();
+                }
+            catch
+                {
+                //MessageBox.Show("Erro ao conectar, verifique se o modulo esta ligado na USB",
+
+                MessageBox.Show("Verifique se o modulo esta ligado na USB", "Erro ao conectar!",
+     MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            if (simconnect == null)
+            {
+                try
+                {
+                    // the constructor is similar to SimConnect_Open in the native API
+                    simconnect = new SimConnect("Managed Data Request", this.Handle, WM_USER_SIMCONNECT, null, 0);
+
+                    initDataRequest();
+                    timer1.Start();
+                }
+                catch (COMException ex)
+                {
+                    displayText("Nao foi possivel conectar ao FSX");
+                }
+            }
+            else
+            {
+                displayText("Error - try again");
+                closeConnection();
+
+                //   setButtons(true, false, false);
+            }
+        }
+
+        void simconnect_OnRecvSimobjectDataBytype(SimConnect sender, SIMCONNECT_RECV_SIMOBJECT_DATA_BYTYPE data)
+        {
+            switch ((DATA_REQUESTS)data.dwRequestID)
+            {
+                case DATA_REQUESTS.REQUEST_1:
+                    Struct1 s1 = (Struct1)data.dwData[0]; //  monta a string s1 para enviar aos textbox
+
+                    // displayText("Title: " + s1.title);
+                    //displayText("Lat:   " + s1.latitude);
+                    //displayText("Lon:   " + s1.longitude);
+                    //displayText("Alt:   " + s1.altitude);
+
+                    //TextBox1("" + s1.x);
+                    //TextBox2("" + s1.x);
+                    //TextBox3("" + s1.y);
+                    //TextBox3("B" + s1.b);
+
+                    // P é o movimento do profundor pitch
+                    if (s1.a >= 0)
+                    {
+                        TextBox1("P" + s1.a);
+                        serialPort1.WriteLine("P"+ s1.a);
+                    }
+
+                    if (s1.a <= 0)
+                    {
+                        TextBox1("P" + s1.a);
+                        serialPort1.WriteLine("P" + s1.a);
+                    }
+
+                    // A é o movimento do aileron pitch
+                    if (s1.b >= 0)
+                    {
+                        TextBox2("A" + s1.b);
+                        serialPort1.WriteLine("A" + s1.b);
+                    }
+
+                    if (s1.b <= 0)
+                    {
+                        TextBox2("A" + s1.b);
+                        serialPort1.WriteLine("A" + s1.b);
+                    }
+                   
+                    break;
+
+                default:
+                    displayText("Unknown request ID: " + data.dwRequestID);
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            serialPort1.Close();
+            button1.Enabled = true;
+            closeConnection();
+            //button2.Enabled = false;
+            //textBox1.ReadOnly = true;
+            //textBox1.Clear();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // The following call returns identical information to:
+            //simconnect.RequestDataOnSimObject(DATA_REQUESTS.REQUEST_1, DEFINITIONS.Struct1, SimConnect.SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD.ONCE);
+            try
+            {
+                simconnect.RequestDataOnSimObjectType(DATA_REQUESTS.REQUEST_1, DEFINITIONS.Struct1, 1, SIMCONNECT_SIMOBJECT_TYPE.USER);
+            }
+            catch
+            {
+                displayText("Erro ja conectado!");
+                timer1.Stop();
+            }
+        }
+
+        // Response number
+        int response = 1;
+
+        // Output text - display a maximum of 10 lines
+        string output = "\n";  // \n para jogar o texto abaixo de 10 linhas
+
+        void displayText(string s)
+        {
+            // remove first string from output
+            output = output.Substring(output.IndexOf("\n") + 1);
+
+            // add the new string
+            output += "" + response++ + ": " + s;
+
+            // display it
+            richTextBox3.Text = output;
+        }
+
+        void TextBox1(string s)
+        {
+            richTextBox1.Text = s;
+        }
+
+        void TextBox2(string s)
+        {
+            richTextBox2.Text = s;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            timer1.Stop();
+            serialPort1.Close();
+            button1.Enabled = true;
+            closeConnection();
+        }
+    }
+}
